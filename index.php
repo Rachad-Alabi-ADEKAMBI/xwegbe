a86eba47694cd201c6eee89f57fef2188fe4841f
Rachad ADEKAMBI <adekambirachad@gmail.com>
2022-01-11 20:16:21 +0000

    Update index.php

--- index.php
+++ index.php
@@ -47,99 +47,218 @@ if (!empty ($_POST)){
     <title>Xwegbe - Accueil</title>
 </head>
 <body>
-    
-   <div class="container" id="bookings" >
+  
         <?php include 'header.php'; ?>
 
-        <div class="top">
-            <h1 class="site_title" >Bienvenue sur <span>XWEGBE !</span></h1>
+        <div class="categories__list__post__item">
+            <div class="row">
+                    <div class="post">
+                        <div class="categories__post__item__pic">
+                            <div class="post__meta">
+                                <h4>
+                                    08
+                                </h4>
 
-            <form action="" method="POST"  id="bookings" class="top__research-form" 
-                    id="reservations">
-          
-                    <label class="top__research-form__label">
-                        Date d'arrivée: <br>
-                        <input type="date" placeholder="Arrivée*" name="arrival_date">
-                    </label>
-
-                    <label class="top__research-form__label">
-                        Date de départ: <br>
-                        <input type="date" placeholder="Départ*" name="departure_date">
-                    </label>
-
-                    <label class="top__research-form__label">
-                        Nombre de personnes: <br>
-                        <select  name="rooms" id="">
-                                <option value="">Sélectionner</option>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                        </select>
-                    </label>
-
-                    <label class="top__research-form__label"> <br>
-                        <button type="submit" class="top__research-form__submit">
-                            Vérifier la disponibilité <i></i>
-                        </button> 
-                        
-                        <i class="fas fa-search look-icon"></i>
-                        
-                    </label>
-            </form>
-
-            <?php  if (!empty($errors)):?>
-
-              <div class="alert" width=400>
-                      <ul>
-                          <?php foreach ($errors as $error): ?>   
-                          <li><?= $error; ?></li>
-                          <?php endforeach;?>
-                      </ul>
-              </div>
-          <?php endif; ?>
-
-        </div>
-
-        <div class="about-section">
-            <h2 class="about-section__title">
-                Xwegbe, 1er catalogue d'appartements meublés au Bénin
-            </h2>
-
-            <div class="about-section__content">
-                <div class="about-section__content__picture">
-                    <div class="background">
-                        <img src="public/images/xwegbe.jpeg" alt="">
+                                <span>
+                                    Aout
+                                </span>
+                            </div>
+                        </div>
                     </div>
-                </div>
-    
-                <div class="about-section__content__details">
-                            <img src="public/icons/people.svg" alt="">
-    
-                            <h3 class="title">
-                                Qui sommes nous ?
-                            </h3>
-    
-                            <p class="text">
-                            Nous sommes une entreprise de locations 
-                            d’appartements meubles a cotonou et environs. 
-                            Nous avons des appartments disponibles pour 
-                            tout type de sejours et pour tous les budgets. 
-                            Merci de nous faire confiance depuis bientot 3 ans. 
-                            </p>
 
-                            <button class="link">
-                                <a href="a-propos.php">En savoir plus</a>
-                            </button>
-                </div>
+                    <div class="post">
+                        <ul>
+                            <li>
+                                OKLm
+                            </li>
+
+                            <li>
+                                Random text
+                            </li>
+                        </ul>
+
+                        <h3>
+                            Titre du post
+                        </h3>
+
+                        <ul class="post__Widget">
+                            <li>
+                                By Admin
+                            </li>
+
+                            <li>
+                                3 min read
+                            </li>
+
+                            <li>
+                                20 comments
+                            </li>
+                        </ul>
+
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur
+                             adipisicing elit. Cum dolorem rerum 
+                             soluta voluptatibus quasi minus dolore 
+                             necessitatibus ipsa voluptatum mollitia 
+                             dolores perferendis incidunt, magnam quod
+                              eveniet repellendus, facere quis illo?
+                        </p>
+                    </div>
             </div>
         </div>
+
+        <div class="container" id="bookings" >
+
+                <div class="top">
+                    <h1 class="site_title" >Bienvenue sur <span>XWEGBE !</span></h1>
+
+                    <form action="" method="POST"  id="bookings" class="top__research-form" 
+                            id="reservations">
+                
+                            <label class="top__research-form__label">
+                                Date d'arrivée: <br>
+                                <input type="date" placeholder="Arrivée*" name="arrival_date">
+                            </label>
+
+                            <label class="top__research-form__label">
+                            <img src="public/icons/people.svg" alt="">
+                        <br>
+                                <input type="date" placeholder="Départ*" name="departure_date">
+                            
+                            </label>
+
+                            <label class="top__research-form__label">
+                                Nombre de personnes: <br>
+                                <select  name="rooms" id="">
+                                        <option value="">Sélectionner</option>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                </select>
+                            </label>
+
+                            <label class="top__research-form__label"> <br>
+                                <button type="submit" class="top__research-form__submit">
+                                    Vérifier la disponibilité <i></i>
+                                </button> 
+                                
+                                <i class="fas fa-search look-icon"></i>
+                                
+                            </label>
+                    </form>
+
+                    <?php  if (!empty($errors)):?>
+
+                    <div class="alert" width=400>
+                            <ul>
+                                <?php foreach ($errors as $error): ?>   
+                                <li><?= $error; ?></li>
+                                <?php endforeach;?>
+                            </ul>
+                    </div>
+                <?php endif; ?>
+
+            </div>
+
+            <div class="about-section">
+                <h2 class="about-section__title">
+                    Xwegbe, 1er catalogue d'appartements meublés au Bénin
+                </h2>
+
+                <div class="about-section__content">
+                    <div class="about-section__content__picture">
+                        <div class="background">
+                            <img src="public/images/xwegbe.jpeg" alt="">
+                        </div>
+                    </div>
+        
+                    <div class="about-section__content__details">
+                                <img src="public/icons/people.svg" alt="">
+        
+                                <h3 class="title">
+                                    Qui sommes nous ?
+                                </h3>
+        
+                                <p class="text">
+                                Nous sommes une entreprise de locations 
+                                d’appartements meubles a cotonou et environs. 
+                                Nous avons des appartments disponibles pour 
+                                tout type de sejours et pour tous les budgets. 
+                                Merci de nous faire confiance depuis bientot 3 ans. 
+                                </p>
+
+                                <button class="link">
+                                    <a href="a-propos.php">En savoir plus</a>
+                                </button>
+                    </div>
+                </div>
+            </div>
         </div>
         
         <?php include 'slider.php'; ?>
   
     <?php include 'slider2.php'; ?>
+
+        <div class="categories__list__post__item">
+            <div class="row">
+                    <div class="post">
+                        <div class="categories__post__item__pic">
+                            <div class="post__meta">
+                                <img src="" alt="">
+                                <h4>
+                                    08
+                                </h4>
+
+                                <span>
+                                    Aout
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="post">
+                        <ul>
+                            <li>
+                                OKLm
+                            </li>
+
+                            <li>
+                                Random text
+                            </li>
+                        </ul>
+
+                        <h3>
+                            Titre du post
+                        </h3>
+
+                        <ul class="post__Widget">
+                            <li>
+                                By Admin
+                            </li>
+
+                            <li>
+                                3 min read
+                            </li>
+
+                            <li>
+                                20 comments
+                            </li>
+                        </ul>
+
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur
+                             adipisicing elit. Cum dolorem rerum 
+                             soluta voluptatibus quasi minus dolore 
+                             necessitatibus ipsa voluptatum mollitia 
+                             dolores perferendis incidunt, magnam quod
+                              eveniet repellendus, facere quis illo?
+                        </p>
+                    </div>
+            </div>
+        </div>
+        
 
     <div class="reasons-section">
         <div class="reasons-section__content">
@@ -147,14 +266,29 @@ if (!empty ($_POST)){
                 <img src="public/icons/hand.svg" alt="">
 
                 <h3 class="title">
-                    Pourquoi nous choisir ? 
+                    Pourquoi nous choisir
                 </h3>
 
                 <p class="text">
-                    Comme en témoignent nos clients précédents, nous avons à 
-                    coeur votre confort et votre sécurité. Dans nos appartements
-                    équipés tout a été pris en compte  pour que votre séjour
-                    se déroule dans les meilleures conditions possibles.
+                    Dans nos appartements, vous vous sentirez comme chez vous, entre autres équipements
+                    vous disposez de: 
+                    <ul>
+                        <li>
+                        <i class="fas fa-check"></i> La machine à laver
+                        </li>
+
+                        <li>
+                        <i class="fas fa-check"></i> La Climatisation
+                        </li>
+
+                        <li>
+                        <i class="fas fa-check"></i> Le parking gratuit
+                        </li>
+                        
+                        <li>
+                        <i class="fas fa-check"></i> Wifi, eau et electricité inclus
+                        </li>
+                    </ul>
                 </p>
 
                 <button class="link">
